cd34e7e0ad48780accb73f976773177b5390841e
yousufrahman <ryousuf766@gmail.com>
2026-04-11 21:48:18 +0600

    added the navlink active mark

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -9,10 +9,10 @@ const Navbar = () => {
                     <Link to={'/'}> <h2 className='font-bold text-3xl'>App store</h2> </Link>
                 </div>
                 <ul className='flex gap-3'>
-                    <li><NavLink to={'/'}>Home</NavLink></li>
-                    <li><NavLink to={'/apps'}>apps</NavLink></li>
-                    <li><NavLink to={'/installedApp'}>Installed App</NavLink></li>
-                    <li><NavLink to={'/error'}>Downloads</NavLink></li>
+                    <li><NavLink to={'/'} className={({isActive}) => isActive ? 'text-green-500 border border-green-500 p-2' : ""}>Home</NavLink></li>
+                    <li><NavLink to={'/apps'} className={({isActive}) => isActive ? 'text-green-500 border border-green-500 p-2' : ""}>apps</NavLink></li>
+                    <li><NavLink to={'/installedApp' } className={({isActive}) => isActive ? 'text-green-500 border border-green-500 p-2' : ""}>Installed App</NavLink></li>
+                    <li><NavLink to={'/error'} className={({isActive}) => isActive ? 'text-green-500 border border-green-500 p-2' : ""}>Downloads</NavLink></li>
                     
                 </ul>
                 <div>
--- src/pages/Apps/App.jsx
+++ src/pages/Apps/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { use } from 'react';
+ const appDataPromise = fetch('/products.json').then(res=>res.json())
 
 const App = () => {
+    const appData = use(appDataPromise)
+    
+
     return (
         <div>
-            app
+            <p>{appData.length}</p>
         </div>
     );
 };
