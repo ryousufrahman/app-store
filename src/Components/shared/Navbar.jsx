c3b4351a0332490d27d4ae6fa8d49a1492df80f2
yousufrahman <ryousuf766@gmail.com>
2026-04-11 13:58:46 +0600

    added the navbar

--- src/Components/shared/Navbar.jsx
+++ src/Components/shared/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router';
 
 const Navbar = () => {
     return (
-        <div>
-            navbar
+        <div className='shadow bg-base-100'>
+            <nav className='max-w-[1200px] mx-auto flex justify-between items-center  mb-5 p-5'>
+                <div>
+                    <Link to={'/'}> <h2 className='font-bold text-3xl'>App store</h2> </Link>
+                </div>
+                <ul className='flex gap-3'>
+                    <li><NavLink to={'/'}>Home</NavLink></li>
+                    <li><NavLink to={'/apps'}>apps</NavLink></li>
+                    <li><NavLink to={'/error'}>Download</NavLink></li>
+                    
+                </ul>
+                <div>
+                    <button className='btn btn-primary'>Support</button>
+                </div>
+
+            </nav>
         </div>
     );
 };
